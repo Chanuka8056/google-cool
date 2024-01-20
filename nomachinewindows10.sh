9b07bcd3ae57f9754f0c32a92152c0a5a2bec3bf
Chanuka8056 <145699429+Chanuka8056@users.noreply.github.com>
2024-01-20 20:55:46 +0530

    Update nomachinewindows10.sh

--- nomachinewindows10.sh
+++ nomachinewindows10.sh
@@ -1,4 +1,4 @@
-wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/Chanuka8056/google-cool/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 
